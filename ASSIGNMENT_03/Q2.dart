67f1e48cc0b281c856eeea41b870a18f3494d561
MuhammadUzain55 <uzainsheikh55@gmail.com>
2025-01-01 17:47:27 +0500

    Update

--- ASSIGNMENT_03/Q2.dart
+++ ASSIGNMENT_03/Q2.dart
@@ -1,8 +1,16 @@
-void main() {
+import 'dart:io';
 
+void main() {
   // Q.2: Implement a code that finds the factorial of a number using a while
   // loop or for loop.
   // Example:
   // Input: 5
   // Output: Factorial of 5 is 120
+
+  var number = int.parse(stdin.readLineSync()!);
+  var factorial = int.parse(stdin.readLineSync()!);
+  for (var i = 1; i <= number; i++) {
+    factorial *= i;
+  }
+  print("factorial of $number is $factorial");
 }
