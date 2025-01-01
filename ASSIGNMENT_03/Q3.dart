26a2d55614ba9779b233e316559bb1ec19b6b0a3
MuhammadUzain55 <uzainsheikh55@gmail.com>
2025-01-01 17:49:04 +0500

    assignment Q.3

---
+++ ASSIGNMENT_03/Q3.dart
@@ -0,0 +1,12 @@
+void main() {
+// Q.3: Write a program that calculates the sum of all the digits in a given
+// number using a while loop.
+// Example:
+// Input: 12345
+// Output: Sum of digits: 15
+
+  int numbers = 12345;
+  while (numbers ) {
+    
+  }
+}
