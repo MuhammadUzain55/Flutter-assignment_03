b574c47565c02c82f3870c43b0b0c6383368b396
MuhammadUzain55 <uzainsheikh55@gmail.com>
2025-01-02 15:37:38 +0500

    Update

--- ASSIGNMENT_03/Q4.dart
+++ ASSIGNMENT_03/Q4.dart
@@ -1,8 +1,18 @@
-void main () {
-
+void main() {
 // Q.4: Implement a code that finds the largest element in a list using a for
 // loop.
 // Example:
 // Input: [3, 9, 1, 6, 4, 2, 8, 5, 7]
 // Output: Largest element: 9
-}
+
+  List numbers = [3, 9, 1, 6, 4, 2, 8, 5, 7];
+  int largest = numbers[0];
+  for (int i in numbers) {
+     if (i > largest) {
+       largest = i;
+     }
+    }
+    print("Largest element: $largest");
+  }
+  
+
